dffae8f4e6d40252f92a5d052705a167e5cd1d81
Arya0D <arya1.dirham1@gmail.com>
2024-06-03 11:50:20 +0700

    update

--- components/page/about.jsx
+++ components/page/about.jsx
@@ -13,7 +13,7 @@ const About = () => {
                     <div className="bg-sky-700 shadow-md p-4 grid gap-4">
                     <div className="text-3xl md:text-6xl font-bold">
                         <div>Programmer &</div>
-                        <div>Tech enthusiast</div>
+                        <div>Tech enthusiast.</div>
                     </div>
                     <p className="text-lg md:text-xl mb-6">Hello My Name is Arya Dirham Wijaya Kusmah i&apos;m a programmer. Studied at Politeknik Negeri Cilacap, informatics engineering study program.</p>
                     <Contact>
